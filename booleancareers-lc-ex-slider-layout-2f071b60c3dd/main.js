9837fdc86d925f879fbe6b5e77de01488f628107
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-27 18:56:01 +0100

    Aggiunto alla funzione anche il cambio di colore delle icone
    Aggiunto alla funzione anche il cambio di colore delle icone

--- booleancareers-lc-ex-slider-layout-2f071b60c3dd/main.js
+++ booleancareers-lc-ex-slider-layout-2f071b60c3dd/main.js
@@ -7,13 +7,17 @@ $(function (){
     //funzione che al click da la classe active all'img successiva e la toglie a quella precedente
     avanti.click(function(){
         $('img.active').next().addClass('active');
+        $('i.active').next().addClass('active');
         $('img.active').prev().removeClass('active');
+        $('i.active').prev().removeClass('active');
     });
 
     //funzione che al click da la classe active all'img precedente e la toglie a quella successiva
     indietro.click(function(){
         $('img.active').prev().addClass('active');
+        $('i.active').prev().addClass('active');
         $('img.active').next().removeClass('active');
+        $('i.active').next().removeClass('active');
     });
 });
         
