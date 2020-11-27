8eddfd6febd7957a533113ccdffa5484970f5ee5
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-27 13:58:16 +0100

    Create 2 variabili per le icone ai lati del carosello
    Create 2 variabili per le icone ai lati del carosello

---
+++ booleancareers-lc-ex-slider-layout-2f071b60c3dd/main.js
@@ -0,0 +1,6 @@
+$(function (){
+    //Variabile collegata all'icona sinistra
+    var indietro = $('.fa-angle-left');
+    //Variabile collegata all'icona destra
+    var avanti = $('.fa-angle-right');
+});
