4e5fbe77749295b085f409f42da666df870fac6a
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-27 18:52:46 +0100

    Create funzioni che fanno scorrere le immagini a destra e sinistra
    Create funzioni che fanno scorrere le immagini a destra e sinistra

--- booleancareers-lc-ex-slider-layout-2f071b60c3dd/main.js
+++ booleancareers-lc-ex-slider-layout-2f071b60c3dd/main.js
@@ -1,6 +1,19 @@
 $(function (){
     //Variabile collegata all'icona sinistra
-    var indietro = $('.fa-angle-left');
+    var indietro = $('div.prev i.fa-angle-left');
     //Variabile collegata all'icona destra
-    var avanti = $('.fa-angle-right');
+    var avanti = $('div.next i.fa-angle-right');
+
+    //funzione che al click da la classe active all'img successiva e la toglie a quella precedente
+    avanti.click(function(){
+        $('img.active').next().addClass('active');
+        $('img.active').prev().removeClass('active');
+    });
+
+    //funzione che al click da la classe active all'img precedente e la toglie a quella successiva
+    indietro.click(function(){
+        $('img.active').prev().addClass('active');
+        $('img.active').next().removeClass('active');
+    });
 });
+        
